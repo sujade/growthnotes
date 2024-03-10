0bc3fa31afb7d5c50942a9b5116a11c34cc1d2b6
sujade <dnmzsu@gmail.com>
2024-03-10 12:13:29 +0100

    edit component

--- src/components/HomeMiddle.tsx
+++ src/components/HomeMiddle.tsx
@@ -9,7 +9,9 @@ const HomeMiddle = () => {
                                  alt='bicycle'></img></Col>
                 <Col md={2}></Col>
                 <Col md={5}><h2>How do we tailor <br/>your note-taking categories?</h2>
-                    <ul className='mt-5 pt-3'>
+                    <h5 className=' mt-3 text-muted'>We customize and personalize <br/>your note-taking
+                    field based on your choices!</h5>
+                    <ul className='mt-3'>
                         <li><h4>General Notes</h4></li>
                         <li><h4>Observations</h4></li>
                         <li><h4>Patterns</h4></li>
