d1208e831d6c26e54571c71a512ec4987f6451ea
sujade <dnmzsu@gmail.com>
2024-03-12 13:14:18 +0100

    added MyNotes and basis for helping tools

--- src/components/NoteWelcome.tsx
+++ src/components/NoteWelcome.tsx
@@ -1,19 +1,19 @@
-import Container from "react-bootstrap/Container";
 import profile from "./Login.tsx";
 import {generateQuote} from "../functions/generateQuote.tsx";
-import Button from "react-bootstrap/Button";
+import {Col, Row} from "react-bootstrap";
 
 const NoteWelcome = () => {
     return (
-            <Container id="#home" className="d-flex flex-row justify-content-evenly mb-5 mt-5">
-                <div className="text-start mb-5 mt-5"><h1>Welcome, {profile.name}! <br/>How are you today?</h1>
-                    <span>{generateQuote()}</span></div>
-                <div className="text-end mb-5 mt-5">
-                    <h1>Ready to start?</h1>
-                    <Button className="btn btn-primary btn-lg">Let's go!</Button></div>
+        <Row className="text-start mb-5 mt-5">
+            <Col>
+                <Row><h2>Welcome, {profile.name}! How are you today?</h2></Row>
+                <Row><span>{generateQuote()}</span></Row>
+            </Col>
 
-                <div></div>
-            </Container>
+            <Col>
+
+            </Col>
+        </Row>
     );
 };
 
--- src/pages/MyNotes.tsx
+++ src/pages/MyNotes.tsx
@@ -1,12 +1,15 @@
-import Navigation from "../components/Navigation.tsx";
-import NoteWelcome from "../components/NoteWelcome.tsx";
 import LoggedFooter from "../components/LoggedFooter.tsx";
+import Container from "react-bootstrap/Container";
+import NavigationNotes from "../components/NavigationNotes.tsx";
+import HelpingTools from "../components/HelpingTools.tsx";
 
 const MyNotes = () => {
     return (
         <div>
-           <Navigation/>
-            <NoteWelcome/>
+           <NavigationNotes/>
+            <Container className='border border-2 border-success rounded-2 mb-5'>
+                <HelpingTools/>
+            </Container>
             <LoggedFooter/>
         </div>
     );
