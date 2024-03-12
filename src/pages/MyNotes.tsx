834bfc70e5265d9929b80f87ef767f942a7262a0
sujade <dnmzsu@gmail.com>
2024-03-12 15:52:22 +0100

    fix background

--- src/pages/MyNotes.tsx
+++ src/pages/MyNotes.tsx
@@ -5,13 +5,16 @@ import {Col, Row} from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import {useState} from "react";
 import NoteBox from "../components/NoteBox.tsx";
-
 const MyNotes = () => {
     const [toggle, setToggle] = useState(false);
 
     const handleClick = () => {
         setToggle(!toggle);
     };
+
+    const triggerError = () => {
+        alert("Something went wrong!")
+    }
 
     return (
         <div>
@@ -21,7 +24,7 @@ const MyNotes = () => {
                     <Col md={2}>
                         <Button onClick={handleClick} className='btn-light btn-outline-primary fw-bold'>New</Button>
                         <span className='p-2'></span>
-                        <Button className='btn-light btn-outline-primary fw-bold'>Previous</Button>
+                        <Button onClick={triggerError} className='btn-light btn-outline-primary fw-bold'>Previous</Button>
                     </Col>
 
                     <Col md={10}></Col>
