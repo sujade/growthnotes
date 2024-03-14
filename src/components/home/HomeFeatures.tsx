672ad0fba32aaf5ca49cca94596f48a15baf3404
sujade <dnmzsu@gmail.com>
2024-03-14 17:07:51 +0100

    optimization of elements on mobile

--- src/components/home/HomeFeatures.tsx
+++ src/components/home/HomeFeatures.tsx
@@ -3,12 +3,11 @@ import Bike from '../../images/bike.jpeg'
 
 const HomeFeatures = () => {
     return (
-        <div className='d-flex align-content-center justify-content-evenly pt-5 pb-5 mb-5'>
+        <div className='container'>
             <Row>
-                <Col md={5}><img className='border border-3 border-black rounded-3' src={Bike}
+                <Col className="d-flex justify-content-center" md={6}><img className='border border-3 border-black rounded-3' src={Bike}
                                  alt='bicycle'></img></Col>
-                <Col md={2}></Col>
-                <Col md={5}><h2>How do we tailor <br/>your note-taking categories?</h2>
+                <Col md={6}><h2 className="mt-5 mt-md-0">How do we tailor <br/>your note-taking categories?</h2>
                     <h5 className='mt-3 text-muted'>We customize and personalize <br/>your note-taking
                     field based on your choices!</h5>
                     <ul className='mt-3'>
--- src/components/home/HomeLogin.tsx
+++ src/components/home/HomeLogin.tsx
@@ -4,7 +4,7 @@ import {GoogleOAuthProvider} from "@react-oauth/google";
 const HomeLogin = () => {
     return (
         <GoogleOAuthProvider clientId="12318472371-j2t13uug1t2lbgg25il8498d4m1d1buh.apps.googleusercontent.com">
-            <div className='text-center pb-4 mb-5'>
+            <div className='text-center mt-5 pb-4 mb-5'>
             <h2>Ready to start?</h2>
                 <p className='text-muted mb-3' >Just login with you Google account and <br/>
                start taking notes on your personal page!</p>
