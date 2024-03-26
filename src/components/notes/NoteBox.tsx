f260524988e5c633413237c611f42545d3ee092b
sujade <dnmzsu@gmail.com>
2024-03-26 10:13:13 +0100

    add mobile optimization for NoteBox.tsx

--- src/components/notes/NoteBox.tsx
+++ src/components/notes/NoteBox.tsx
@@ -6,7 +6,7 @@ function NoteBox() {
     return (
         <Form className="m-5">
             <Row>
-                <Col md={2}>
+                <Col sm={12} md={2}>
                     <Form.Group className="mb-3" controlId="formDate">
                         <Form.Label className="fw-bold">Date</Form.Label>
                         <Form.Control type="date"/>
@@ -14,40 +14,40 @@ function NoteBox() {
             </Row>
 
             <Row>
-                <Col>
+                <Col sm={12} md={6}>
                     <Form.Group className="mb-3" controlId="formSubject">
                         <Form.Label className="fw-bold">Subject</Form.Label>
                         <Form.Control type="text" placeholder="What was your session about?"/>
                     </Form.Group></Col>
 
-                <Col>
+                <Col sm={12} md={6}>
                     <Form.Group className="mb-3" controlId="formInformation">
                         <Form.Label className="fw-bold">New Information</Form.Label>
-                        <Form.Control type="text" placeholder="What have you learned during your session?"/>
+                        <Form.Control type="text" placeholder="What have you learned?"/>
                     </Form.Group></Col>
             </Row>
 
             <Row>
-                <Col>
+                <Col sm={12} md={6}>
                     <Form.Group className="mb-3" controlId="formHomeworks">
                         <Form.Label className="fw-bold">Homeworks</Form.Label>
                         <Form.Control type="text"
-                                      placeholder="Do you have any therapy homeworks? If yes, note them down here."/>
+                                      placeholder="What are your homrworks?"/>
                     </Form.Group></Col>
 
-                <Col>
+                <Col sm={12} md={6}>
                     <Form.Group className="mb-3" controlId="forOpinion">
                         <Form.Label className="fw-bold">Therapy Opinions</Form.Label>
-                        <Form.Control type="text" placeholder="How did your therapy go? What do you feel about it?"/>
+                        <Form.Control type="text" placeholder="How did your therapy go?"/>
                     </Form.Group></Col>
             </Row>
 
             <Row>
-                <Col md={6}>
+                <Col sm={12} md={6}>
                     <Form.Group className="mb-3" controlId="forFree">
                         <Form.Label className="fw-bold">Free Input</Form.Label>
                         <Form.Control type="text"
-                                      placeholder="Is there anything else you would like to add?"/>
+                                      placeholder="Anything else to add??"/>
                     </Form.Group></Col>
             </Row>
 
